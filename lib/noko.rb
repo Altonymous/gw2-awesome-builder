7d5cc0087c9f8cb872f719370ef1a3b741041840
Kevin Altman <kevin@thinkrelevance.com>
2012-11-10 15:54:34 -0500

    page scraping for gw2db

--- lib/noko.rb
+++ lib/noko.rb
@@ -3,27 +3,66 @@ class Noko
   require 'open-uri'
 
   def initialize
+    @base_url = "http://www.gw2db.com/items/"
+    @base_params = "&filter-item-rarity=6&filter-item-minimum-required-level=80&filter-item-maximum-required-level=80"
+
+    @weight = {'light' => 1, 'medium' => '3','heavy' => 2}
   end
 
-  def self.get_stuff
-    doc = Nokogiri::HTML(open("http://www.gw2db.com/items/armor/coats?filter-filter-item-type=3&filter-item-rarity=6&filter-item-armor-weight-type=2&filter-item-armor-type=3&filter-item-minimum-required-level=80&filter-item-maximum-required-level=80"))
+  def get_items(category, type, weight)
+    doc = Nokogiri::HTML(open("http://www.gw2db.com/items/#{category}/#{type}/?filter-item-armor-weight-type=#{@weight[weight]}#{@base_params}"))
+    # puts "http://www.gw2db.com/items/#{category}/#{type}/?filter-item-armor-weight-type=#{@weight[weight]}#{@base_params}"
 
-    items = {}
+    items = Hash.new
 
     doc.css('.gwitem').map do |link|
       url = "http://www.gw2db.com#{link['href']}"
-
       page = Nokogiri::HTML(open url)
-      puts page.css('.db-defense').text
-      # stat = page.css('.db-stat').content
 
-      # items.merge({:name => link.content, :href => url, :defense => defense, :stat => stat})
+
+      item = Hash.new
+
+      # Get Item Name
+      item['name'] = page.css('header h2.caption').first.text
+      item = item.merge(get_stat(page.css('.db-defense')))
+
+      puts
+
+      page.css('.db-stat').each do |stat|
+        stats = get_stat(stat)
+        item = item.merge(stats)
+      end
+
+      puts "Name: #{item["name"]} -----"
+      puts "Defense: #{item["defense"]}"
+      puts "Power: #{item["power"]}"
+      puts "Vitality: #{item["vitality"]}"
+      puts "Precision: #{item["precision"]}"
+      puts "Condition Damage: #{item["condition"]}"
+      puts "Toughness: #{item["toughness"]}"
+      puts "Critical Damage: #{item["critical_damage"]}"
+      puts "Critical Chance: #{item["critical_chance"]}"
+      puts "Healing: #{item["healing"]}"
+      puts "End -----"
+
     end
 
     # items.each do |item|
     #   item
     #   #
     # end
+  end
+
+  def get_stat(stat)
+    stat_name = stat.text.match(/\w+[a-zA-Z]/).to_s
+    stat_name = stat_name.parameterize.underscore
+    stat_value = stat.text.match(/\w+[0-9]/).to_s
+
+    {stat_name => stat_value}
+  end
+
+  def format_name
+    self.name.gsub!(/( )/, '_').downcase!
   end
 end
 
