2259538fca1066f651aa343ae922805a7798b43b
Kevin Altman <kevin@itsthatguy.com>
2012-11-13 18:21:35 -0500

    styling and fancy javascript stuffs for highlighting rows, and handling rounded corners while sorting

--- app/helpers/outfits_helper.rb
+++ app/helpers/outfits_helper.rb
@@ -3,7 +3,11 @@ module OutfitsHelper
     capture_haml do
       haml_tag "div", {:data => {:name => name.parameterize}, :class => "slider-range", :style => "height: 250px;"}
       haml_tag :div, {:class => "slider-amount"} do
-        haml_tag :div, name, {:class => "label"}
+        haml_tag :div, {:class => "label"} do
+          haml_tag :div, {:class => "table"} do
+            haml_tag :div, name, {:class => "table-cell"}
+          end
+        end
         haml_tag :div, {:id => "slider-" + name.parameterize, :class => "amount"}
       end
     end
